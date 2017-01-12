bc9ac89f0fb59c118104ca405f553676039dcbd4
Husam <yugimotoo@windowslive.com>
2017-01-12 07:24:58 +0300

    Further fixing

--- ACMEstudentPortal/StudentList.aspx.designer.cs
+++ ACMEstudentPortal/StudentList.aspx.designer.cs
@@ -31,6 +31,24 @@ namespace ACMEstudentPortal {
         protected global::System.Web.UI.WebControls.GridView studentsGrid;
         
         /// <summary>
+        /// exportPDF control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button exportPDF;
+        
+        /// <summary>
+        /// exportJSON control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button exportJSON;
+        
+        /// <summary>
         /// studentsList control.
         /// </summary>
         /// <remarks>
